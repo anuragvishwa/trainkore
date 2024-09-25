efb9e8ae711ad17ebca94d72d228401f3e9387e0
Anurag Vishwa <anuragvishwacd@gmail.com>
2024-09-25 13:03:05 +0530

    added microsoft clarity

--- app/(default)/layout.tsx
+++ app/(default)/layout.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 import { useEffect } from "react";
-import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+
 
 import AOS from "aos";
 import "aos/dist/aos.css";
@@ -21,14 +20,26 @@ export default function DefaultLayout({
       duration: 600,
       easing: "ease-out-sine",
     });
-  });
+
+    (function (c: { [key: string]: any }, l: Document, a: string, r: string, i: string, t: HTMLScriptElement | null, y: HTMLElement | null) {
+      c[a] = c[a] || function () {
+        (c[a].q = c[a].q || []).push(arguments);
+      };
+      t = l.createElement(r) as HTMLScriptElement;
+      t.async = true;
+      t.src = "https://www.clarity.ms/tag/" + i;
+      y = l.getElementsByTagName(r)[0] as HTMLElement;
+      y.parentNode?.insertBefore(t, y);
+    })(window as any, document, "clarity", "script", "o8mq4vhre8", null, null);
+  }, []);
+
+ 
 
   return (
     <>
       <main className="relative flex grow flex-col">{children}</main>     
       <Footer />
-      <Analytics />
-      <SpeedInsights />
+
     </>
   );
 }
